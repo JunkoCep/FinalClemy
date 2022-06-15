e88600bbfb888bd705157cee519e8cc96222d20c
JunkoCep <esteban.junco.cepeda@gmail.com>
2022-06-14 23:06:47 -0500

    hola mundo

---
+++ src/main/java/org/springframework/security/crypto/bcrypt/BCryptPasswordEncoder.java
@@ -0,0 +1,10 @@
+package org.springframework.security.crypto.bcrypt;
+
+public class BCryptPasswordEncoder {
+
+	public String encode(String newPassword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
